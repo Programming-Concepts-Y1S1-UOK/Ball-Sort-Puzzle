1f0d981c4053e9ba4f92ed7156f7409382b20627
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-10-13 00:45:19 +0530

    add basic array handle function

--- main.cpp
+++ main.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
-int main() {
-  cout << "Hello, World!";
+void setArrayValuesToZero(int array[4][5]);
+void PrintArrayValuesToConsole(int array[4][5]);
 
+int main() {
+  int ballPipeLine[4][5];
+  setArrayValuesToZero(ballPipeLine);
+  PrintArrayValuesToConsole(ballPipeLine);
+}
+
+void setArrayValuesToZero(int array[4][5]) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 5; j++) {
+      array[i][j] = 0;
+    }
+  }
+}
+
+void PrintArrayValuesToConsole(int array[4][5]) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 5; j++) {
+      cout << array[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
