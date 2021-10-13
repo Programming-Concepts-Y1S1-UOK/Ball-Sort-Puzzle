2c50f10ca24f26d7aeed5fc10d698ea61621ec65
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-10-13 10:01:40 +0530

    add setArrayColValues function

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 void setArrayValuesToZero(int array[4][5]);
 void PrintArrayValuesToConsole(int array[4][5]);
+void setArrayColValues(int array[4][5], int colSize, int arrayOrder[12]);
 
 int main() {
   int ballPipeLine[4][5];
+  int arrayOrderLevelOne[12] = {1, 2, 1, 2, 2, 1, 1, 3, 3, 3, 3, 2};
   setArrayValuesToZero(ballPipeLine);
+  PrintArrayValuesToConsole(ballPipeLine);
+  setArrayColValues(ballPipeLine, 3, arrayOrderLevelOne);
   PrintArrayValuesToConsole(ballPipeLine);
 }
 
@@ -25,5 +29,17 @@ void PrintArrayValuesToConsole(int array[4][5]) {
       cout << array[i][j] << " ";
     }
     cout << endl;
+  }
+}
+void setArrayColValues(int array[4][5], int colSize, int arrayOrder[12]) {
+  // yellow 1
+  // blue 2
+  // red 3
+  int count = 0;
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < colSize; j++) {
+      array[i][j] = arrayOrder[count];
+      count++;
+    }
   }
 }
